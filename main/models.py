32887cac50dca17a4d17ef59605e53128df4bcb4
KhayruRP <khayru.rafamanda@ui.ac.id>
2025-09-09 00:12:56 +0700

    commit 1 - tugas 2

---
+++ main/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+import uuid
+from django.db import models
+
+class News(models.Model):
+    CATEGORY_CHOICES = [
+        ('jersey', 'Jersey'),
+        ('shoes', 'Shoes'),
+        ('socks', 'Socks'),
+        ('shorts', 'Shorts'),
+        ('new', 'New')
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='new')
+    thumbnail = models.URLField(blank=True, null=True)
+    store_views = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+    @property
+    def is_item_hot(self):
+        return self.store_views > 20
+        
+    def increment_views(self):
+        self.store_views += 1
+        self.save()
