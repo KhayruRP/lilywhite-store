57832a9ca9bb1a7ff58a886745f3b64b039fb5b0
KhayruRP <khayru.rafamanda@ui.ac.id>
2025-09-09 13:48:44 +0700

    Commit 3 - tugas2

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    data = {
+        'Aplikasi' : 'Lilywhite Store',
+        'Name': 'Khayru RafamandaPrananta',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", data)
