afc96a1f135a7731e535d21d5238807cff17e3a0
KhayruRP <khayru.rafamanda@ui.ac.id>
2025-09-17 16:46:23 +0700

    Commit 8 - Tugas 3

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,11 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('', show_main, name='show_main'),
+    path('create-items/', create_items, name='create_items'),
+    path('news/<str:id>/', show_items, name='show_items'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+     path('xml/<str:items_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:items_id>/', show_json_by_id, name='show_json_by_id'),
 ]
